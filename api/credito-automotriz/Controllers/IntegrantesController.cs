e8537a8197746815b31d97bb9e9d07d8912afaa7
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2021-03-08 09:04:15 -0700

    Comenzando con la api
    Se comenzó a hacer testing a los endpoints para tomar practica y
    comenzar a elaborar los endpoints necesarios.

---
+++ api/credito-automotriz/Controllers/IntegrantesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace credito_automotriz.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IntegrantesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> ObtenerIntegrantes()
+        {
+            var integrantes = new List<string> { "Paul", "Jorge" };
+            return integrantes;
+        }
+        [HttpGet("[action]")]
+        public  IActionResult CrearNuevoIntegrante()
+        {
+            return Ok("Se creó con exito!");
+        }
+    }
+}
