66176a0fc0267e7b38c82ee4edb7f7320444db1e
PAUL VELIZ <paulsystem32@gmail.com>
2021-03-21 00:34:28 -0700

    Formulario solicitud
    Added:
    	*Seleccion de hijos.

--- src/src/app/solicita-credito-page/solicita-credito-page.component.ts
+++ src/src/app/solicita-credito-page/solicita-credito-page.component.ts
@@ -8,12 +8,13 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 })
 export class SolicitaCreditoPageComponent implements OnInit {
 
-  
+  private paso: Boolean = false; // false: pagina1 == true: pagina2
 
   constructor(private renderer: Renderer2, private http:HttpClient) { 
   }
   
   ngOnInit(): void {
+    const gotHijos = document.querySelector('#gotHijos');
     const filePicker = <HTMLInputElement>document.querySelector('#image-picker');
     const imageInput = <HTMLInputElement>document.querySelector('#image-input');
     this.renderer.listen(filePicker, 'change', (event) => {
@@ -32,6 +33,12 @@ export class SolicitaCreditoPageComponent implements OnInit {
         imageInput.src = response.secure_url;
       })
     });
+
+    this.renderer.listen(gotHijos, 'change', (event) => {
+      console.warn((event.target.value == "true") ? 'Tiene Hijos...' : 'No tiene hijos');
+      let cantHijosSelector = document.querySelector('.cantidad-hijos');
+      this.renderer.setStyle(cantHijosSelector, 'display', `${(event.target.value == "true") ? 'block' : 'none'}`);
+    });
   }
 
   mostrarSeleccionImagen(mostrar: Boolean): void{
@@ -39,6 +46,7 @@ export class SolicitaCreditoPageComponent implements OnInit {
     const formulario = document.querySelector('.formulario');
     this.renderer.setStyle(subidaImagenSection, 'display', `${(mostrar ? 'flex' : 'none')}`);
     this.renderer.setStyle(formulario, 'animation', `${(mostrar ? 'grow1 1.5s forwards' : 'grow0 1.5s forwards')}`)
+    this.paso = mostrar;
   }
 
   mostrarFormHijos(mostrar: Boolean): void{
@@ -102,7 +110,13 @@ export class SolicitaCreditoPageComponent implements OnInit {
     }
 
     // Esconder hijos y mostrar subida de imagen.
-    this.mostrarFormHijos(false);
+    if(!this.paso){
+      console.warn('Pasando al paso de registro #2');
+      this.mostrarFormHijos(false);
+      return;
+    }
+    console.warn('Enviando informacion a la API.');
+
 
   }
 
