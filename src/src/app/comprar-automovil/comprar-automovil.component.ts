18b316fb06671ddbeb713e42b1a70867ace67783
Paul Veliz <paulsystem32@gmail.com>
2021-04-28 15:15:53 -0600

    Report Service + generar reporte

--- src/src/app/comprar-automovil/comprar-automovil.component.ts
+++ src/src/app/comprar-automovil/comprar-automovil.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Automovil } from '../interfaces/automovil.interface';
 import { FinanciarResponse } from '../models/financiarResponse.interface';
 import { AutomovilesService } from '../services/automoviles.service';
+import { ReportesService } from '../services/reportes.service';
 
 @Component({
   selector: 'app-comprar-automovil',
@@ -13,15 +14,25 @@ export class ComprarAutomovilComponent implements OnInit {
 
   public automovil:Automovil | null = null;
   public autoFinanciado:FinanciarResponse | null = null;
+  private planId:number = 0;
+  private clienteId:number = 0;
+  private automovilId:number = 0;
 
   constructor(private route:ActivatedRoute,
-              private apiAutos:AutomovilesService) { }
+              // private router:Router,
+              private apiAutos:AutomovilesService,
+              private apiReport:ReportesService
+              ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe( params => {
       console.log(params.clienteId);
       console.log(params.automovilId);
+      console.log(params.planId);
       // plan id
+      this.planId = params.planId;
+      this.clienteId = params.clienteId;
+      this.automovilId = params.automovilId;
 
       this.apiAutos.ObtenerAutomovilId(<number>params.automovilId).subscribe( automovil => {
         this.automovil = automovil;
@@ -32,6 +43,12 @@ export class ComprarAutomovilComponent implements OnInit {
       });
 
     });
+  }
+  comprarAuto(){
+      this.apiReport.GenerarReporteEnganche(this.automovilId, this.planId, this.clienteId)
+                    .subscribe( pdf => {
+                      window.open(pdf);
+                    });
   }
 
 
--- src/src/app/services/reportes.service.ts
+++ src/src/app/services/reportes.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,4 +9,12 @@ import { environment } from 'src/environments/environment';
 export class ReportesService {
 
   constructor(private http:HttpClient) { }
+
+  GenerarReporteEnganche(automovilId:number, planId:number, clienteId:number):Observable<any>{
+    return this.http.post<any>(`${environment.apiUrl}/api/report/enganche`, {
+      url: `reporte/enganche/automovil/${automovilId}/plan/${planId}/cliente/${clienteId}`
+    });
+  }
+
 }
+
