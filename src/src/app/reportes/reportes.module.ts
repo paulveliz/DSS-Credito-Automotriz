5245d4ee9233b9d6167c62a08c987ec61e978d25
Paul Veliz <paulsystem32@gmail.com>
2021-05-10 13:04:17 -0600

    Abono
    Todo lo necesario para generar abono quedo listo.

--- src/src/app/reportes/reportes.module.ts
+++ src/src/app/reportes/reportes.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FichaPagoComponent } from './ficha-pago/ficha-pago.component';
+import { FichaAbonoComponent } from './ficha-abono/ficha-abono.component';
 
 
 
 @NgModule({
   declarations: [
-    FichaPagoComponent
+    FichaPagoComponent,
+    FichaAbonoComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     FichaPagoComponent,
+    FichaAbonoComponent
   ]
 })
 export class ReportesModule { }
